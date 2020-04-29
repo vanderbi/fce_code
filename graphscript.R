f4a82e9423a5e66200266cfdb357a9b199715efa
vanderbi <krvander@fiu.edu>
2020-04-29 15:05:26 -0600

    This is my first commit of this script

---
+++ graphscript.R
@@ -0,0 +1,22 @@
+install.packages("tidyverse")
+library(tidyverse)
+
+#National Parks in California
+ca <- read_csv("https://raw.githubusercontent.com/OHI-Science/data-science-training/master/data/ca.csv") 
+
+#Acadia National Park
+acadia <- read_csv("https://raw.githubusercontent.com/OHI-Science/data-science-training/master/data/acadia.csv")
+
+#Southeast US National Parks
+se <- read_csv("https://raw.githubusercontent.com/OHI-Science/data-science-training/master/data/se.csv")
+
+#2016 Visitation for all Pacific West National Parks
+visit_16 <- read_csv("https://raw.githubusercontent.com/OHI-Science/data-science-training/master/data/visit_16.csv")
+
+#All Nationally designated sites in Massachusetts
+mass <- read_csv("https://raw.githubusercontent.com/OHI-Science/data-science-training/master/data/mass.csv")
+
+head(ca)
+
+ggplot(data = ca) + 
+  geom_point(aes(x = year, y = visitors))
